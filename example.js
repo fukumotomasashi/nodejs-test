499d7709f3f76612648f5e1ffe14d3c7b587c72f
fukumotomasashi <cvb25362@gmail.com>
2020-10-20 20:29:27 +0900

    commit

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
-var http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var http = require('http'); //httpモジュール呼び出し
+var server = http.createServer(function (request, response) {
+    // リクエストを受けると以下のレスポンスを送信する
+    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+    response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
+    response.end(); // レスポンス送信を完了する
+});
+server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
