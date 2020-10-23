57ad4c1d066edb124e5a720b62ecb212637d90b8
fukumotomasashi <cvb25362@gmail.com>
2020-10-24 07:21:54 +0900

    modify port

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ var http = require('http'); //httpモジュール呼び出し
 var express = require("express");
 var app = express();
 var path = require("path")
-var server = http.createServer(function (request, response) {
-    // リクエストを受けると以下のレスポンスを送信する
-    // response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
-    // response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
-		// response.end(); // レスポンス送信を完了する
-		app.set('port', (process.env.Port || 8000))
-		// テンプレートが格納されているフォルダを指定する
-		app.set('views', path.join(__dirname, './views/'))
-		// ビューエンジンにejsを設定
-		app.set('view engine', 'ejs');
-		// ルートにアクセスした時にindex.ejsを返す設定
-		app.get("/", function(req, res, next){
-			res.render("index");
-		});
-});
-server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
+// var server = http.createServer(function (request, response) {
+//     // リクエストを受けると以下のレスポンスを送信する
+//     // response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+//     // response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
+// 		// response.end(); // レスポンス送信を完了する
+// 		app.set('port', (process.env.Port || 8000))
+// 		// テンプレートが格納されているフォルダを指定する
+// 		app.set('views', path.join(__dirname, './views/'))
+// 		// ビューエンジンにejsを設定
+// 		app.set('view engine', 'ejs');
+// 		// ルートにアクセスした時にindex.ejsを返す設定
+// 		app.get("/", function(req, res, next){
+// 			res.render("index");
+// 		});
+// });
+// server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
 
 // 'use strcit';
 
@@ -60,17 +60,17 @@ server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
 // var app = express();
 // var path = require("path")
 
-// app.set('port', (process.env.Port || 8000))
+app.set('port', (process.env.Port || 5000))
 
-// // テンプレートが格納されているフォルダを指定する
-// app.set('views', path.join(__dirname, './views/'))
-// // ビューエンジンにejsを設定
-// app.set('view engine', 'ejs');
-// // ルートにアクセスした時にindex.ejsを返す設定
-// app.get("/", function(req, res, next){
-// 	res.render("index");
-// });
+// テンプレートが格納されているフォルダを指定する
+app.set('views', path.join(__dirname, './views/'))
+// ビューエンジンにejsを設定
+app.set('view engine', 'ejs');
+// ルートにアクセスした時にindex.ejsを返す設定
+app.get("/", function(req, res, next){
+	res.render("index");
+});
 
-// app.listen(app.get('port'), function() {
-//   console.log("App is running on port :" + app.get('port'))
-// });
+app.listen(app.get('port'), function() {
+  console.log("App is running on port :" + app.get('port'))
+});
