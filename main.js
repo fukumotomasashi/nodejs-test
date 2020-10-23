fec308e89caacb4f5c563a9f8586bd5e8a99ed2f
fukumotomasashi <cvb25362@gmail.com>
2020-10-24 07:09:10 +0900

    modify main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 var http = require('http'); //httpモジュール呼び出し
+var express = require("express");
+var app = express();
+var path = require("path")
 var server = http.createServer(function (request, response) {
     // リクエストを受けると以下のレスポンスを送信する
-    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
-    response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
-    response.end(); // レスポンス送信を完了する
+    // response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+    // response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
+		// response.end(); // レスポンス送信を完了する
+		app.set('port', (process.env.Port || 8000))
+		// テンプレートが格納されているフォルダを指定する
+		app.set('views', path.join(__dirname, './views/'))
+		// ビューエンジンにejsを設定
+		app.set('view engine', 'ejs');
+		// ルートにアクセスした時にindex.ejsを返す設定
+		app.get("/", function(req, res, next){
+			res.render("index");
+		});
 });
 server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
 
