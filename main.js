7fc9b885d6f7058abe8fad58d04485b3f3305db2
fukumotomasashi <cvb25362@gmail.com>
2020-10-27 06:21:22 +0900

    modify cors

--- main.js
+++ main.js
@@ -1,67 +1,29 @@
 var http = require('http'); //httpモジュール呼び出し
 var express = require("express");
 var app = express();
-var path = require("path")
+var path = require("path");
+var cors = require("cors");
 var messageRouter = require('./routes/message');
 const PORT = process.env.PORT || 5000
+// const allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type, Authorization, access_token'
+//   )
 
+//   // intercept OPTIONS method
+//   if ('OPTIONS' === req.method) {
+//     res.send(200)
+//   } else {
+//     next()
+//   }
+// }
+
+// app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/', messageRouter);
-// app.get('/', (req, res) => res.render('index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-// var server = http.createServer(function (request, response) {
-//     // リクエストを受けると以下のレスポンスを送信する
-//     // response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
-//     // response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
-// 		// response.end(); // レスポンス送信を完了する
-// 		app.set('port', (process.env.Port || 8000))
-// 		// テンプレートが格納されているフォルダを指定する
-// 		app.set('views', path.join(__dirname, './views/'))
-// 		// ビューエンジンにejsを設定
-// 		app.set('view engine', 'ejs');
-// 		// ルートにアクセスした時にindex.ejsを返す設定
-// 		app.get("/", function(req, res, next){
-// 			res.render("index");
-// 		});
-// });
-// server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
-
-// 'use strcit';
-
-// const axios = require('axios');
-// const qs = require('querystring');
-// const BASE_URL = 'https://notify-api.line.me';
-// const PATH =  '/api/notify';
-// const LINE_TOKEN = `O8T0r4dDkWbO5y9ujb3Sg6F3j63zoD8wdEgSTLPzo1c`;
-
-// let config = {
-//     baseURL: BASE_URL,
-//     url: PATH,
-//     method: 'post',
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         'Authorization': `Bearer ${LINE_TOKEN}`
-//     },
-//     data: qs.stringify({
-//         message: `こんばんわ`,
-//     })
-// };
-
-// axios.request(config)
-// .then((res) => {
-//     console.log(res.status);
-// })
-// .catch((error) => {
-//     console.log(error);
-// });
-
-// const lineNotify = require('line-notify-nodejs')('O8T0r4dDkWbO5y9ujb3Sg6F3j63zoD8wdEgSTLPzo1c')
-
-// lineNotify.notify({
-// 	message: '健闘を祈る',
-// }).then(() => {
-// 	console.log('send completed!')
-// });
--- routes/message.js
+++ routes/message.js
@@ -1,6 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const qs = require('querystring');
 
-router.get('/', (req, res) => res.render('index'));
+router.get('/', (req, res) => res.render('message/index'));
+router.post('/post', (req, res) => {
+	const BASE_URL = 'https://notify-api.line.me';
+	const PATH =  '/api/notify';
+	const LINE_TOKEN = `O8T0r4dDkWbO5y9ujb3Sg6F3j63zoD8wdEgSTLPzo1c`;
+
+	let config = {
+			baseURL: BASE_URL,
+			url: PATH,
+			method: 'post',
+			headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Authorization': `Bearer ${LINE_TOKEN}`
+			},
+			data: qs.stringify({
+					message: `こんばんわ`,
+			})
+	};
+
+	axios.request(config)
+	.then((res) => {
+			console.log(res.status);
+	})
+	.catch((error) => {
+			console.log(error);
+	});
+
+	const lineNotify = require('line-notify-nodejs')('O8T0r4dDkWbO5y9ujb3Sg6F3j63zoD8wdEgSTLPzo1c')
+
+	lineNotify.notify({
+		message: '第３ハードルのテスト',
+	}).then(() => {
+		console.log('send completed!')
+	});
+});
 
 module.exports = router;
